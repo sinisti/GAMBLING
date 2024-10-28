32f8f910d18c13e7580ccbbbdb5d28767cf75f59
Sinisti! <Spideralan2008@gmail.com>
2024-10-28 21:13:44 +0100

    General Updates

--- app/src/main/java/com/example/gambling/MainActivity.java
+++ app/src/main/java/com/example/gambling/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.gambling;
 import static java.lang.Math.random;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -11,11 +10,15 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private TextView Kostka1;
+    private int random1 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Kostka1 = findViewById(R.id.Kostka1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int random1 = new Random(6).nextInt();
+
+        Kostka1 = findViewById(R.id.Kostka1);
+        random1 = (int)(Math.random() * 6 + 1);
+
         Kostka1.setText(Integer.toString(random1));
-    }}
+    }
+}
