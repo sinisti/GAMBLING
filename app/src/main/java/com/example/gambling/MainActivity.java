a050bb5235c613ae0192111b00daf39efa3a09e3
Sinisti! <Spideralan2008@gmail.com>
2024-10-28 19:51:28 +0100

    masz adam

--- app/src/main/java/com/example/gambling/MainActivity.java
+++ app/src/main/java/com/example/gambling/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.gambling;
+import static java.lang.Math.random;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -7,18 +10,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 public class MainActivity extends AppCompatActivity {
+    private TextView Kostka1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Kostka1 = findViewById(R.id.Kostka1);
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
-int random1 = rand.nextInt(100) + 1; //generate random numbers from 1-100
-
-n1.setText(Integer.toString(random1)); //n1 is a TextView
+        int random1 = new Random(6).nextInt();
+        Kostka1.setText(Integer.toString(random1));
+    }}
